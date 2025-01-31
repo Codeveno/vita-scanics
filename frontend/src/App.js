60b69bd031579790737bbaf31b839792edacc7b6
codeveno <samsonochieng476@gmail.com>
2025-01-31 13:43:50 +0300

    minor error corection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/telemedicine" element={<TelemedicineSession />} />
       </Routes>
     </div>
-  );    `                                                                                                                                                                       ````````````````
+  ); 
 };
 
 export default App;
